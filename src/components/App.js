98936895606133504a1335ce9f050ba99e1f4eb2
Suyesh Bhandari <suyeshb@gmail.com>
2017-07-06 17:28:09 -0400

    Lesson 16

---
+++ src/components/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish = fish => {
+    const fishes = {
+      ...this.state.fishes
+    };
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    this.setState({fishes: fishes});
+  }
+
+  loadSamples = () => {
+    this.setState({fishes: sampleFishes});
+  }
+
+  addToOrder=(key)=>{
+    const order = {...this.state.order};
+    order[key] = order[key] + 1 || 1;
+    this.setState({order: order});
+  }
+
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market"/>
+          <ul className="list-of-fishes">
+            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} index={key}/>)}
+          </ul>
+        </div>
+        <Order/>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Order.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Order extends React.Component {
+  render(){
+    return(<h1>Order</h1>);
+  }
+}
+
+export default Order;
