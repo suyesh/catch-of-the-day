ac15829adf313cbe7aa0ac3723d3f1d88aa3bd09
Suyesh Bhandari <suyeshb@gmail.com>
2017-07-06 17:44:24 -0400

    Lesson 17

--- src/components/App.js
+++ src/components/App.js
@@ -42,7 +42,7 @@ class App extends React.Component {
             {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} index={key}/>)}
           </ul>
         </div>
-        <Order/>
+        <Order fishes={this.state.fishes} order={this.state.order}/>
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     );
